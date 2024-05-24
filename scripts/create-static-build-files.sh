8ffe2e66ff3c238ac40cfd098595280cf2716914
Tamas Mohos <zerosuxx@gmail.com>
2024-05-24 09:18:47 +0200

    fix: move php files to public folder

--- scripts/create-static-build-files.sh
+++ scripts/create-static-build-files.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-mkdir -p static-build-files
+mkdir -p static-build-files/public
 cat config/php/*.ini > static-build-files/php.ini
-cp *.php static-build-files/
+cp *.php static-build-files/public
