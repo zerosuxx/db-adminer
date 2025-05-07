3a1e5e4a1d0c3820de7bb519fadb70c0417e04fe
Tamas Mohos <zerosuxx@gmail.com>
2025-05-08 01:03:19 +0200

    fix: patch

--- scripts/apply-patches.sh
+++ scripts/apply-patches.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
+set -exo pipefail
 
-patch caddy/frankenphp/main.go < <(diff -u <(curl -fs "https://raw.githubusercontent.com/dunglas/frankenphp/refs/tags/v${FRANKENPHP_VERSION:-"1.5.0"}/caddy/frankenphp/main.go") ./caddy/frankenphp/main.go)
+patch caddy/frankenphp/main.go < <(diff -u <(curl -fsS "https://raw.githubusercontent.com/dunglas/frankenphp/refs/tags/v${FRANKENPHP_VERSION:-1.5.0}/caddy/frankenphp/main.go") ./caddy/frankenphp/main.go)
