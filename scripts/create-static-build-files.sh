ca78274c6feec0df079e2ac5aecffa82fd81d271
Tamas Mohos <zerosuxx@gmail.com>
2025-05-08 00:42:35 +0200

    fix: file copy

--- scripts/create-static-build-files.sh
+++ scripts/create-static-build-files.sh
@@ -3,7 +3,7 @@
 set -x
 
 mkdir -p static-build-files/public
-cp public/ static-build-files/public
+cp -R public/ static-build-files/public
 adminer_version=${ADMINER_VERSION:-"5.3.0"}
 curl -f -L -o static-build-files/public/adminer.php "https://github.com/vrana/adminer/releases/download/v${adminer_version}/adminer-${adminer_version}.php"
 
