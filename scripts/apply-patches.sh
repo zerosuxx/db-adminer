e9588c8460ce5348d1a14526c7c03e30b2bddf1e
Tamas Mohos <zerosuxx@gmail.com>
2025-05-07 22:20:17 +0200

    fix: build

--- scripts/apply-patches.sh
+++ scripts/apply-patches.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-patch caddy/frankenphp/main.go < <(diff -u <(curl -s https://raw.githubusercontent.com/dunglas/frankenphp/refs/tags/v${FRANKENPHP_VERSION:-'1.5.0'}/caddy/frankenphp/main.go) ./caddy/frankenphp/main.go)
+patch caddy/frankenphp/main.go < <(diff -u <(curl -fs "https://raw.githubusercontent.com/dunglas/frankenphp/refs/tags/v${FRANKENPHP_VERSION:-"1.5.0"}/caddy/frankenphp/main.go") ./caddy/frankenphp/main.go)
--- scripts/create-static-build-files.sh
+++ scripts/create-static-build-files.sh
@@ -4,8 +4,8 @@ set -x
 
 mkdir -p static-build-files/public
 cp public/ static-build-files/public
-adminer_version=${ADMINER_VERSION:-'5.3.0'}
-curl -L -o static-build-files/public/adminer.php "https://github.com/vrana/adminer/releases/download/v${adminer_version}/adminer-${adminer_version}.php"
+adminer_version=${ADMINER_VERSION:-"5.3.0"}
+curl -f -L -o static-build-files/public/adminer.php "https://github.com/vrana/adminer/releases/download/v${adminer_version}/adminer-${adminer_version}.php"
 
 cat config/php/*.ini > static-build-files/php.ini
 cp composer.* static-build-files/
